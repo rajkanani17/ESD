36f905a5057075daa2692034a50c7934522e738e
rajkanani17 <KananiRaj.Hemantbhai@iiitb.ac.in>
2024-11-20 11:08:17 +0530

    Update Task 2 of query regarding top 2 products

--- ESD_YummyApp/src/main/java/com/rajkanani/esd_yummyapp/repo/ProductRepo.java
+++ ESD_YummyApp/src/main/java/com/rajkanani/esd_yummyapp/repo/ProductRepo.java
@@ -11,6 +11,6 @@ public interface ProductRepo extends JpaRepository<Product, Long> {
 
     Optional<Product> findByName(String name);
 
-    @Query("SELECT p FROM Product p WHERE p.price BETWEEN 15 AND 30 ORDER BY p.price")
+    @Query("SELECT p FROM Product p WHERE p.price BETWEEN 15 AND 30 ORDER BY p.price LIMIT 2")
     List<Product> findTop2ByPriceBetweenOrderByPriceAsc();
 }
